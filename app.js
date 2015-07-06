4c5fafce43bade3d1c05d3bf6be239d41eb3b92a
ecandela <erik.candela.rojas@gmail.com>
2015-07-05 22:09:49 -0500

    a

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var bson = require('bson');
+//var bson = require('bson');
 
 // Database
-var mongo = require('mongodb');
+//var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/test');
+var db = monk('ecandela:ecandela@ds047642.mongolab.com:47642/user');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
